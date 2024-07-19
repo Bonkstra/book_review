17dbe90754340f45770074bd86a28918df01a0d0
ONLINQ\bink <bink@onlinq.nl>
2024-07-19 11:15:16 +0200

    Book controller and index

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,15 @@
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 // INDEX
 Route::get('/', function () {
-    return "HOME";
+    return redirect()->route('books.index');
 })->name('home');
+
+// BOOK
+Route::resource('books', App\Http\Controllers\BookController::class);
 
 // FALLBACK
 Route::fallback(function () {
