551171a9b0b53540f6b44474c6efb6120359f5c7
ONLINQ\bink <bink@onlinq.nl>
2024-07-19 13:57:58 +0200

    Caching

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Book;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class BookController extends Controller
 {
@@ -24,10 +25,10 @@ class BookController extends Controller
             'highest_rated_last_week' => $books->highestRatedLastWeek(),
             'highest_rated_last_moth' => $books->highestRatedLastMonth(),
             'highest_rated_all_time' => $books->highestRatedAllTime(),
-            default => $books->latest(),
+            default => $books->popular()->minReviews(2)->HighestRated()->latest(),
         };
 
-        $books = $books->get();
+        $books = Cache::remember('books:' . $filter . ':' . $title, 3600, fn () => $books->get());
 
         return view('books.index', ['books' => $books]);
     }
@@ -51,9 +52,13 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        $book = Cache::remember('book:' . $book->id, 3600, fn () => $book->load([
+            'reviews' => fn ($query) => $query->latest(),
+        ]));
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Support\Facades\Cache;
 
 class Review extends Model
 {
@@ -15,5 +16,12 @@ class Review extends Model
     public function book()
     {
         return $this->belongsTo(Book::class);
+    }
+
+    protected static function booted()
+    {
+        // IF A REVIEW IS UPDATED OR DELETED, DELETE THE BOOK CACHE
+        static::updated(fn (Review $review) => Cache::forget('book:' . $review->book->id));
+        static::deleted(fn (Review $review) => Cache::forget('book:' . $review->book->id));
     }
 }
